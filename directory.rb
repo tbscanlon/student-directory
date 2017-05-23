d49b4be9862f98458571d4ed85c58c80b89f06a0
tbscanlon <thomas.scanlon@outlook.com>
2017-05-23 15:30:08 +0100

    Complete step 10

--- directory.rb
+++ directory.rb
@@ -1,15 +1,17 @@
+@students = [] # an ampty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students."
   puts "To finish, just hit return twice"
-  students = [] # create an empty array
+  #students = [] # create an empty array
   name = gets.chomp # get the first name
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp # get another name from the user
   end
-  students # return the array of students
+  #students # return the array of students
 end
 
 def print_header
@@ -17,40 +19,45 @@ def print_header
   puts "--------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def interactive_menu
-  students = []
   loop do
-    # 1. Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-    # 2. Read the input and save it into a variable
-    selection = gets.chomp
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-    # 3. Do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
   end
 end
 
